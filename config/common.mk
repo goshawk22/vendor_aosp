deccc4ae17c5977cdc3c6efa9bc90e3b130cb40a
ElDainosor <eldainosor@gmail.com>
2018-11-05 14:22:04 +0530

    Add more qs tiles styles [2/2]
    Change-Id: I3ed890ff214cc70b7287517dc630ad6d3f6dcddb
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -263,6 +263,11 @@ PRODUCT_PACKAGES += \
     QStileCircleTrim \
     QStileDefault \
     QStileDualToneCircle \
-    QStileSquircleTrim
+    QStileSquircleTrim \
+    QStileAttemptMountain \
+    QStileDottedCircle \
+    QStileNinja \
+    QStilePokesign \
+    QStileWavey
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
