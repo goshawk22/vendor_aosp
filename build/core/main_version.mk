ab4e22ca5c925056d61600d27c2d0f8a07f34d0c
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-08-03 18:20:10 +0530

    branding: Bump version to 2.5

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -19,7 +19,7 @@ endif
 
 # Branding
 BLASTER_BUILD_TYPE ?= UNOFFICIAL
-BLASTER_VERSION ?= 2.0
+BLASTER_VERSION ?= 2.5
 
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
