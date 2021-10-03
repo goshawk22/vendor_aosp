5b59fe3958d8c2e56b5e40f44d4792b4e09c668c
Adam Lawson <adamjlawson22@gmail.com>
2021-10-03 15:40:15 +0100

    vendor: Support microG build
    If WITH_MICROG is set to true, build will be with microG services

---
+++ config/microg.mk
@@ -0,0 +1,13 @@
+# microG packages
+PRODUCT_PACKAGES += \
+	FDroid \
+	FDroidPrivilegedExtension \
+	FakeStore \
+	GmsCore \
+	GsfProxy \
+	MozillaNlpBackend \
+	NominatimNlpBackend \
+	additional_repos.xml \
+	com.google.android.maps.jar
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/microg
